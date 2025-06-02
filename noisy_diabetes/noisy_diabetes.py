d8f832e337c8db0fc8b3f4900a38e18f15c4ca35
Hiroyuki Akama <38000971+hilolani@users.noreply.github.com>
2025-06-02 10:52:20 +0900

    Update noisy_diabetes.py

--- noisy_diabetes/noisy_diabetes.py
+++ noisy_diabetes/noisy_diabetes.py
@@ -5,6 +5,14 @@ import csv
 import random
 from sklearn.datasets import load_diabetes
 from sklearn.utils import Bunch
+try:
+    # Python 3.9+
+    from importlib.resources import files
+except ImportError:
+    # Python 3.7–3.8
+    from importlib_resources import files
+from . import data  # noisy_diabetes/data/
+
 
 def add_gaussian_noise_np(X, mean=0.0, sd=0.1):
     X = np.array(X)
@@ -23,7 +31,7 @@ def add_uniform_noise_np(X, low=-0.01, high=0.01):
 def load_noisy_diabetes(return_X_y=False, as_frame=False, scaled=True):
     diabetes = load_diabetes(return_X_y=False, as_frame=as_frame, scaled=scaled)
 
-    with open('/content/drive/My Drive/Colab Notebooks/noisy_diabetes_data.csv') as f:
+    with files(data).joinpath("noisy_diabetes_data.csv").open("r", encoding="utf-8") as f:
         reader = csv.reader(f)
         l = [row for row in reader]
     f.close()
@@ -47,7 +55,7 @@ def load_noisy_diabetes(return_X_y=False, as_frame=False, scaled=True):
 def load_total_noisy_diabetes(return_X_y=False, as_frame=False, scaled=True):
     diabetes = load_diabetes(return_X_y=False, as_frame=as_frame, scaled=scaled)
 
-    with open('/content/drive/My Drive/Colab Notebooks/total_noisy_diabetes_data.csv') as f:
+    with files(data).joinpath("total_noisy_diabetes_data.csv").open("r", encoding="utf-8") as f:
         reader = csv.reader(f)
         l = [row for row in reader]
     f.close()
